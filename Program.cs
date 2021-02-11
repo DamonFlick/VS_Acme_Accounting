8209b21961f4ca6f63139d6417520d84a5222e05
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-11 12:53:06 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcom to Acme Accounting Systems \nRemember, we're \"accounting \" on you!");
+            Console.Read();
+        }
+    }
+
